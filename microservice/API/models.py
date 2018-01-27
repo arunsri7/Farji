eb7835f432154344051419cc7d7d079e1401edf9
Arjun Mahishi <arjun.mahishi@gmail.com>
2018-01-27 16:36:24 +0530

    Added NewsItem model

--- microservice/API/models.py
+++ microservice/API/models.py
@@ -1,6 +1,12 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class NewsItem(models.Model):
+	author = models.CharField(max_length=30)
+	title = models.CharField(max_length=100)
+	content = models.CharField(max_length=2000)
+	source = models.CharField(max_length=200)
+
+	def __str__(self):
+		return "%d. %s" % (self.id, self.author)
