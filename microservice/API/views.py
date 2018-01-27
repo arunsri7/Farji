5843f43b275c77b8d88b4cd39d9f069163f293ee
Arjun Mahishi <arjun.mahishi@gmail.com>
2018-01-28 02:15:45 +0530

    Implemented scoring

--- microservice/API/views.py
+++ microservice/API/views.py
@@ -17,7 +17,7 @@ from newsapi import NewsApiClient
 # initilize the news API
 newsapi = NewsApiClient(api_key='3111edae78f6492983bd0a6df945356e')
 
-sources = "crypto-coins-news,bbc-sport,google-news-au,rte,google-news-it,business-insider,google-news,hacker-news,the-sport-bible,recode,info-money,bleacher-report,financial-times,la-nacion,gruenderszene,bbc-news,four-four-two,la-gaceta,ynet,the-guardian-au,mtv-news-uk,google-news-fr,nhl-news,entertainment-weekly,wired-de,news-com-au,google-news-sa,news24,independent,rtl-nieuws,the-next-web,the-economist,google-news-ca,cbc-news,the-new-york-times,the-telegraph,sabq,nfl-news,espn-cric-info,fox-news,national-geographic,axios,vice-news,the-washington-post,reuters,ary-news,bloomberg,fox-sports,la-repubblica,espn,google-news-is,lequipe,business-insider-uk,google-news-ru,msnbc,google-news-in,google-news-uk,metro,techcrunch-cn,google-news-ar,aftenposten,nrk,wirtschafts-woche,cbs-news,xinhua-net,newsweek,liberation,goteborgs-posten,handelsblatt,the-guardian-uk,associated-press,talksport,mashable,rbc,rt,ansa,fortune,cnbc,al-jazeera-english,the-wall-street-journal,football-italia,buzzfeed,the-globe-and-mail,the-huffington-post,cnn-es,reddit-r-all,google-news-br,lenta,techradar,argaam,mirror,engadget,the-hill,wired,abc-news-au,il-sole-24-ore,nbc-news,ars-technica,politico,infobae,abc-news,cnn,blasting-news-br,svenska-dagbladet,the-times-of-india,medical-news-today,time,focus,usa-today,t3n,daily-mail,the-lad-bible,el-mundo,les-echos,new-york-magazine,le-monde,polygon,ign,new-scientist,mtv-news,the-hindu,spiegel-online,globo,marca,techcrunch,the-irish-times,next-big-future,bild,der-tagesspiegel,australian-financial-review,financial-post,the-verge,die-zeit,breitbart-news";
+publishers = "crypto-coins-news,bbc-sport,google-news-au,rte,google-news-it,business-insider,google-news,hacker-news,the-sport-bible,recode,info-money,bleacher-report,financial-times,la-nacion,gruenderszene,bbc-news,four-four-two,la-gaceta,ynet,the-guardian-au,mtv-news-uk,google-news-fr,nhl-news,entertainment-weekly,wired-de,news-com-au,google-news-sa,news24,independent,rtl-nieuws,the-next-web,the-economist,google-news-ca,cbc-news,the-new-york-times,the-telegraph,sabq,nfl-news,espn-cric-info,fox-news,national-geographic,axios,vice-news,the-washington-post,reuters,ary-news,bloomberg,fox-sports,la-repubblica,espn,google-news-is,lequipe,business-insider-uk,google-news-ru,msnbc,google-news-in,google-news-uk,metro,techcrunch-cn,google-news-ar,aftenposten,nrk,wirtschafts-woche,cbs-news,xinhua-net,newsweek,liberation,goteborgs-posten,handelsblatt,the-guardian-uk,associated-press,talksport,mashable,rbc,rt,ansa,fortune,cnbc,al-jazeera-english,the-wall-street-journal,football-italia,buzzfeed,the-globe-and-mail,the-huffington-post,cnn-es,reddit-r-all,google-news-br,lenta,techradar,argaam,mirror,engadget,the-hill,wired,abc-news-au,il-sole-24-ore,nbc-news,ars-technica,politico,infobae,abc-news,cnn,blasting-news-br,svenska-dagbladet,the-times-of-india,medical-news-today,time,focus,usa-today,t3n,daily-mail,the-lad-bible,el-mundo,les-echos,new-york-magazine,le-monde,polygon,ign,new-scientist,mtv-news,the-hindu,spiegel-online,globo,marca,techcrunch,the-irish-times,next-big-future,bild,der-tagesspiegel,australian-financial-review,financial-post,the-verge,die-zeit,breitbart-news";
 
 @api_view(['POST'])
 @permission_classes([])
@@ -26,15 +26,15 @@ def NewsItems(request):
     """Returns a JSON object, discribing the complete validation on the given news item"""
 
     if request.method == 'POST':
-        # try:
+        try:
             payload = request.POST['newsText'].replace(".", "").replace(" ", ", ")
             
             items = newsapi.get_everything(
             	q=payload,
-            	sources=sources
+            	sources=publishers
             )
 
-            aScore = items['totalResults']
+            aScore = (items['totalResults']/(publishers.count(",")+1))*100
             sources = [{
             	"name": item["source"]["name"], 
             	"url": item["url"],
@@ -42,14 +42,14 @@ def NewsItems(request):
             } for item in items["articles"]]
 
             return Response({
+            	"no_of_sources": items['totalResults'],
             	"aScore": aScore,
             	"newsText": request.POST['newsText'],
             	"sources": sources,
-            	"status": 200,
-            	"res": items
+            	"status": 200
             })
-        # except KeyError:
-            # return Response({"errorMessage": "required parameters are not met", "post": request.POST})
+        except KeyError:
+            return Response({"errorMessage": "required parameters are not met", "post": request.POST})
     return Response({"errorMessage": "Wrong request is being used"})
 
 class ReportNews(CreateAPIView):
